ab741a5a365d68c18cef6b66b15ffdfc8d580b05
CI5KO <hector_oliva16k@hotmail.com>
2022-11-04 12:33:12 -0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '576ef20e-6bc7-4b4f-bb83-34b8312c8d96';
+export const MODAL_CLOSE_SEC = 2.5;
